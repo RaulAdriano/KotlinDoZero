0d47a5de426a35d29f50c8014ca5cf063e063273
Raul Adriano Ramos <raul.ramos@spassu.com.br>
2024-04-09 08:54:50 -0300

    variaveis e constantes

---
+++ app/src/main/java/com/rauladrianoramos/kotlindozero/VariaveisConstantes.kt
@@ -0,0 +1,40 @@
+package com.rauladrianoramos.kotlindozero
+
+const val nomeUsuario = "raulAdriano" // constante tempo de compilação - global
+
+fun retornaNomeUsuario(){
+
+}
+
+class Usuario(){
+
+}
+
+
+fun main (){
+    /*
+    * VARIAVEIS
+    * var  =  posso mudar o valor
+    * val  = constante
+    * */
+
+
+    val nome =  "Raul" // constante tempo de execução
+    val nomeRetorno =  retornaNomeUsuario()
+    val usuario = Usuario()
+
+    var idade = 29
+    idade = 30
+
+    println(nome)
+    println(idade)
+
+
+    val numero1 = 20;
+    val numero2 = 10;
+    val resultado = numero1 + numero2
+
+    val numeroInteiro : Int = 10;
+
+    println(resultado)
+}
